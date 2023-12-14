3cff8d05478888e93bbef0cab49cf1403bcc368e
Podili Avinash Rao <avinash_it@yahoo.com>
2023-12-14 23:31:43 +0530

    Update todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,35 @@
 */
 
 class Todo {
+    constructor() {
+        this.todos = [];
+    }
 
+    add(todo) {
+        this.todos.push(todo);
+    }
+
+    remove(index) {
+        this.todos.splice(index, 1);
+    }
+
+    update(index, updatedTodo) {
+        if (this.todos[index]) {
+            this.todos[index] = updatedTodo;
+        }
+    }
+
+    getAll() {
+        return this.todos;
+    }
+
+    get(index) {
+        return this.todos[index] ?? null;
+    }
+
+    clear() {
+        this.todos = [];
+    }
 }
 
 module.exports = Todo;
