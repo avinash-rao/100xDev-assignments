eb7669b09d4a4f18b84b8126e97455ada0839500
Podili Avinash Rao <avinash_it@yahoo.com>
2023-12-24 16:22:07 +0530

    Update 02-ratelimitter.js

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,20 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req, res, next) {
+    const userId = req.headers['user-id'];
+    if (numberOfRequestsForUser[userId]) {
+        numberOfRequestsForUser[userId]++;
+    } else {
+        numberOfRequestsForUser[userId] = 1;
+    }
+
+    if (numberOfRequestsForUser[userId] > 5) {
+        return res.status(404).send('no entry');
+    }
+    next();
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
